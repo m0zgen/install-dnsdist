270c8d420f1e2e350aba4769c2f6336b87d81402
Yevgeniy Goncharov <yevgeniy.goncharov@gcorelabs.com>
2022-08-22 11:29:22 +0200

    + quiet mode

--- install.sh
+++ install.sh
@@ -9,17 +9,40 @@ set -e
 PATH=$PATH:/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
 SCRIPT_PATH=$(cd `dirname "${BASH_SOURCE[0]}"` && pwd)
 
+# Functions
+# ---------------------------------------------------\
+
+# Help information
+usage() {
+
+    echo -e "\nArguments:
+    -q (use unattended installation with default DPKG config)"
+    exit 1
+
+}
+
+# Checks arguments
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        -q|--quiet) _Q=1; ;;
+        -h|--help) usage ;; 
+        *) echo "Unknown parameter passed: $1"; exit 1 ;;
+    esac
+    shift
+done# Functions
+
 # Init official repo
 # ---------------------------------------------------\
 
 # keep your currently-installed version
-yes N | dpkg --configure -a
+# yes N | dpkg --configure -a
 
-# Install key
-apt update; apt -y install gnupg
-curl https://repo.powerdns.com/FD380FBB-pub.asc | sudo apt-key add -
+install_repos() {
+    # Install key
+    apt update; apt -y install gnupg
+    curl https://repo.powerdns.com/FD380FBB-pub.asc | sudo apt-key add -
 
-# Install repos
+    # Install repos
 cat > /etc/apt/sources.list.d/pdns.list <<_EOF_
 deb [arch=amd64] http://repo.powerdns.com/debian bullseye-dnsdist-17 main
 _EOF_
@@ -29,12 +52,28 @@ Package: dnsdist*
 Pin: origin repo.powerdns.com
 Pin-Priority: 600
 _EOF_
+}
 
-# General install
-apt update
-apt -y install dnsdist
+install_dnsdist() {
+    # General install
+    apt update
+    apt -y install dnsdist
+}
 
-# Final
-echo -e "Done!"
-dnsdist --version
+install_dnsdist_quiet() {
+    export DEBIAN_FRONTEND=noninteractive
+    apt-get -yq installdnsdist
+}
 
+
+final() {
+    # Final
+    echo -e "Done!"
+    dnsdist --version
+}
+
+if [[ "$_Q" -eq "1" ]]; then
+    install_dnsdist_quiet
+else
+    install_dnsdist
+fi
